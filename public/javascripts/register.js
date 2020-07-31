d74b14b780242b393d564d80fa584ffa5b4d6563
eesayas <briones@ualberta.ca>
2020-07-30 23:03:58 -0600

    fixed calendar and added time slots (frontend)

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,9 +1,23 @@
+_.templateSettings = {
+    escape: /\{\{\-(.+?)\}\}/g,
+    evaluate: /\{\%(.+?)\%\}/g,
+    interpolate: /\{\{(.+?)\}\}/g,
+};
+
 $('.calendar-cont').clndr({
+    template: $('#calendar-template').html(),
     clickEvents:{
         click: function(target){
-            console.log(target);
+            if(!Array.from(target.element.classList).includes('select-day')){
+
+                //add circular indicator
+                $('.day-inner').removeClass('select-day'); //remove from others
+                target.element.firstElementChild.classList.toggle('select-day'); //add to target
+                
+                //change selected date like state change
+                $('#selected-date').text(moment(target.date._d).format('MMMM DD'));
+                $('#time-slots').removeClass('hidden'); //show slots
+            }
         }
     }
-    
 });
-
